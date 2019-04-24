b0c8c5516c2b61e4a815e1764cdf04a739a42920
Henrik Bengtsson <hb@aroma-project.org>
2019-04-23 21:10:06 -0700

    Add support for 'progressr.times' [#6]

--- R/handler.R
+++ R/handler.R
@@ -1,17 +1,29 @@
 #' @export
-ascii_alert_handler <- function(symbol = "\a", ..., file = stderr(), enable = interactive()) {
+ascii_alert_handler <- function(symbol = "\a", ..., times = getOption("progressr.times", +Inf), file = stderr(), enable = interactive()) {
   pb <- NULL
-
-  if (enable) {
+  
+  if (!enable) times <- 0
+  if (times > 0) {
+    at <- NULL
+    step <- 0L
+    
     handler <- function(p) {
       stopifnot(inherits(p, "progression"))
+      times <<- times - 1
       type <- p$type
       if (type == "setup") {
-        cat(file = file, symbol)
+        if (is.finite(times)) {
+	  at <<- seq(from = 1L, to = p$steps, length.out = times)
+	}
+        if (times >= 1) cat(file = file, symbol)
       } else if (type == "done") {
         cat(file = file, symbol)
       } else if (type == "update") {
-        cat(file = file, symbol)
+        step <<- step + p$amount
+        if (is.infinite(times) || step >= at[1]) {
+	  at <<- at[-1]
+          cat(file = file, symbol)
+	}
       } else {
         warning("Unknown 'progression' type: ", sQuote(type))
       }
@@ -25,26 +37,41 @@ ascii_alert_handler <- function(symbol = "\a", ..., file = stderr(), enable = in
 
 
 #' @export
-txtprogressbar_handler <- function(..., file = stderr()) {
+txtprogressbar_handler <- function(..., times = getOption("progressr.times", +Inf), file = stderr()) {
   pb <- NULL
 
   ## Import functions
   txtProgressBar <- utils::txtProgressBar
   getTxtProgressBar <- utils::getTxtProgressBar
   setTxtProgressBar <- utils::setTxtProgressBar
-  
-  handler <- function(p) {
-    stopifnot(inherits(p, "progression"))
-    type <- p$type
-    if (type == "setup") {
-      pb <<- txtProgressBar(max = p$steps, ..., file = file)
-    } else if (type == "done") {
-      close(pb)
-    } else if (type == "update") {
-      setTxtProgressBar(pb, value = getTxtProgressBar(pb) + p$amount)
-    } else {
-      warning("Unknown 'progression' type: ", sQuote(type))
+
+  if (times >= 1L) {
+    at <- NULL
+    step <- 0L
+    
+    handler <- function(p) {
+      stopifnot(inherits(p, "progression"))
+      times <<- times - 1L
+      type <- p$type
+      if (type == "setup") {
+        if (is.finite(times)) {
+	  at <<- seq(from = 1L, to = p$steps, length.out = times)
+	}
+	pb <<- txtProgressBar(max = p$steps, ..., file = file)
+      } else if (type == "done") {
+        close(pb)
+      } else if (type == "update") {
+        step <<- step + p$amount
+        if (is.infinite(times) || step >= at[1]) {
+	  at <<- at[-1]
+          setTxtProgressBar(pb, value = step)
+	}
+      } else {
+        warning("Unknown 'progression' type: ", sQuote(type))
+      }
     }
+  } else {
+    handler <- function(p) NULL
   }
   class(handler) <- c("progression_handler", class(handler))
   handler
@@ -53,24 +80,36 @@ txtprogressbar_handler <- function(..., file = stderr()) {
 
 
 #' @export
-tkprogressbar_handler <- function(..., enable = interactive()) {
+tkprogressbar_handler <- function(..., times = getOption("progressr.times", +Inf), enable = interactive()) {
   pb <- NULL
 
-  if (enable) {
+  if (!enable) times <- 0
+  if (times > 0) {
     ## Import functions
     tkProgressBar <- tcltk::tkProgressBar
     getTkProgressBar <- tcltk::getTkProgressBar
     setTkProgressBar <- tcltk::setTkProgressBar
-  
+
+    at <- NULL
+    step <- 0L
+
     handler <- function(p) {
       stopifnot(inherits(p, "progression"))
+      times <<- times - 1
       type <- p$type
       if (type == "setup") {
+        if (is.finite(times)) {
+	  at <<- seq(from = 1L, to = p$steps, length.out = times)
+	}
         pb <<- tkProgressBar(max = p$steps, ...)
       } else if (type == "done") {
         close(pb)
       } else if (type == "update") {
-        setTkProgressBar(pb, value = getTkProgressBar(pb) + p$amount)
+        step <<- step + p$amount
+        if (is.infinite(times) || step >= at[1]) {
+	  at <<- at[-1]
+          setTkProgressBar(pb, value = step)
+	}
       } else {
         warning("Unknown 'progression' type: ", sQuote(type))
       }
@@ -86,23 +125,40 @@ tkprogressbar_handler <- function(..., enable = interactive()) {
 
 
 #' @export
-progress_handler <- function(...) {
+progress_handler <- function(..., times = getOption("progressr.times", +Inf)) {
   pb <- NULL
 
   ## Import functions
   progress_bar <- progress::progress_bar
   
-  handler <- function(p) {
-    stopifnot(inherits(p, "progression"))
-    type <- p$type
-    if (type == "setup") {
-      pb <<- progress_bar$new(total = p$steps, ...)
-    } else if (type == "done") {
-    } else if (type == "update") {
-      pb$tick(len = p$amount)
-    } else {
-      warning("Unknown 'progression' type: ", sQuote(type))
+  if (times > 0) {
+    at <- NULL
+    step <- 0L
+    max <- NULL
+    
+    handler <- function(p) {
+      stopifnot(inherits(p, "progression"))
+      times <<- times - 1
+      type <- p$type
+      if (type == "setup") {
+        max <<- p$steps
+        if (is.finite(times)) {
+	  at <<- seq(from = 1L, to = max, length.out = times)
+	}
+        pb <<- progress_bar$new(total = max, ...)
+      } else if (type == "done") {
+      } else if (type == "update") {
+        step <<- step + p$amount
+        if (is.infinite(times) || step >= at[1]) {
+	  at <<- at[-1]
+          pb$update(ratio = step/max)
+	}
+      } else {
+        warning("Unknown 'progression' type: ", sQuote(type))
+      }
     }
+  } else {
+    handler <- function(p) NULL
   }
   class(handler) <- c("progression_handler", class(handler))
   handler
@@ -111,22 +167,34 @@ progress_handler <- function(...) {
 
 
 #' @export
-beepr_handler <- function(setup = 2L, update = 10L,  done = 11L, enable = interactive(), ...) {
+beepr_handler <- function(setup = 2L, update = 10L,  done = 11L, times = getOption("progressr.times", +Inf), enable = interactive(), ...) {
   pb <- NULL
-
-  if (enable) {
+  
+  if (!enable) times <- 0
+  if (times > 0L) {
     ## Import functions
     beep <- beepr::beep
     
+    at <- NULL
+    step <- 0L
+    
     handler <- function(p) {
       stopifnot(inherits(p, "progression"))
+      times <<- times - 1
       type <- p$type
       if (type == "setup") {
-        beep(setup)
+        if (is.finite(times)) {
+	  at <<- seq(from = 1L, to = p$steps, length.out = times)
+	}
+        if (times >= 1) beep(setup)
       } else if (type == "done") {
         beep(done)
       } else if (type == "update") {
-        beep(update)
+        step <<- step + p$amount
+        if (is.infinite(times) || step >= at[1]) {
+	  at <<- at[-1]
+          beep(update)
+	}
       } else {
         warning("Unknown 'progression' type: ", sQuote(type))
       }
--- tests/with_progress.R
+++ tests/with_progress.R
@@ -1,6 +1,7 @@
 library(progressr)
 
 options(progressr.handler = NULL)
+options(progressr.times = 5L)
 
 message("with_progress() ...")
 
@@ -24,40 +25,9 @@ my_sum <- function(x = 1:10) {
 x <- 1:20
 truth <- sum(x)
 
-message("with_progress() - void ...")
-
-## Mute progress updates
-with_progress({
-  sum <- my_sum(x)
-}, handler = NULL)
-print(sum)
-stopifnot(sum == truth)
-
-message("with_progress() - void ... done")
-
-message("with_progress() - alert ...")
-
-## Mute progress updates
-with_progress({
-  sum <- my_sum(x)
-}, handler = ascii_alert_handler)
-print(sum)
-stopifnot(sum == truth)
-
-message("with_progress() - alert ... done")
-
-
 message("with_progress() - utils::txtProgressBar() ...")
 
 if (requireNamespace("utils")) {
-  ## Display progress using default handler
-  with_progress({
-    sum <- my_sum(x)
-  })
-  print(sum)
-  stopifnot(sum == truth)
-  
-  ## Display progress using default handler
   with_progress({
     sum <- my_sum(x)
   }, handler = txtprogressbar_handler)
@@ -66,6 +36,19 @@ if (requireNamespace("utils")) {
 }
 
 message("with_progress() - utils::txtProgressBar() ... done")
+
+
+message("with_progress() - default ...")
+
+if (requireNamespace("utils")) {
+  with_progress({
+    sum <- my_sum(x)
+  })
+  print(sum)
+  stopifnot(sum == truth)
+}
+
+message("with_progress() - default ... done")
 
 message("with_progress() - tcltk::tkProgressBar() ...")
 
@@ -92,6 +75,18 @@ if (requireNamespace("progress")) {
 message("with_progress() - progress::progress_bar() ... done")
 
 
+message("with_progress() - alert ...")
+
+## Mute progress updates
+with_progress({
+  sum <- my_sum(x)
+}, handler = ascii_alert_handler)
+print(sum)
+stopifnot(sum == truth)
+
+message("with_progress() - alert ... done")
+
+
 message("with_progress() - beepr::beep() ...")
 
 ## Mute progress updates
@@ -102,6 +97,18 @@ print(sum)
 stopifnot(sum == truth)
 
 message("with_progress() - beepr::beep() ... done")
+
+
+message("with_progress() - void ...")
+
+## Mute progress updates
+with_progress({
+  sum <- my_sum(x)
+}, handler = NULL)
+print(sum)
+stopifnot(sum == truth)
+
+message("with_progress() - void ... done")
 
 
 message("with_progress() ... done")
