c00de64103cf7acf0ca98f62d34c3d01a411b2c0
Henrik Bengtsson <hb@aroma-project.org>
2019-05-18 14:16:46 -0700

    TESTS: Testing odds and ends of progression_handler() [covr: 86%]

--- tests/debug.R
+++ tests/debug.R
@@ -3,7 +3,7 @@ library(progressr)
 options(progressr.debug = TRUE)
 options(progressr.enable = TRUE)
 options(progressr.times = +Inf)
-options(progressr.interval = 0)
+options(progressr.interval = 0.1)
 options(delay = 0.0)
 
 message("with_progress() - progressr.debug = TRUE ...")
@@ -15,9 +15,11 @@ with_progress({
 with_progress({
   progress <- progressor(steps = 1 + 2 + 1)
   relay_progress <- progress_aggregator(progress)
-  progress()
+  p <- progress()
+  progressr::progress(p) ## duplicated - will be ignored
   relay_progress(slow_sum(1:2))
-  progress()
+  progress(type = "finish")
+  progress() ## one too many - will be ignored
 })
 
 message("with_progress() - progressr.debug = TRUE ... done")
