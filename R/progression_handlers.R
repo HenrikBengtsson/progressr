110659c7403aa08360a96cf3b0af5d7a7c9206a4
Henrik Bengtsson <hb@aroma-project.org>
2019-05-18 23:50:49 -0700

    Add filesize_handler progression handler [#14]

--- R/progression_handlers.R
+++ R/progression_handlers.R
@@ -419,7 +419,7 @@ progress_handler <- function(format = "[:bar] :percent :message", show_after = 0
         make_pb(format = format, total = config$max_steps,
                 clear = config$clear, show_after = config$enable_after)
         reporter$update(config = config, state = state, progression = progression, ...)
-        if (config$clear) pb$update(1.0)
+        if (config$clear && !pb$finished) pb$update(1.0)
       }
     )
   })
@@ -612,4 +612,72 @@ newline_handler <- function(symbol = "\n", file = stderr(), intrusiveness = getO
   })
   
   progression_handler("newline", reporter, intrusiveness = intrusiveness, ...)
+}
+
+
+
+
+#' Progression Updates Reflected as the Size off a File
+#'
+#' @inheritParams progression_handler
+#'
+#' @param file (character) A filename.
+#'
+#' @param \ldots Additional arguments passed to [progression_handler()].
+#'
+#' @examples
+#' \donttest{\dontrun{
+#' options(progressr.handlers = filesize_handler(file = "myscript.progress"))
+#' with_progress(y <- slow_sum(1:100))
+#' }}
+#'
+#' @importFrom utils file_test
+#' @export
+filesize_handler <- function(file = "default.progress", intrusiveness = getOption("progressr.intrusiveness.file", 5), ...) {
+  reporter <- local({
+    set_file_size <- function(config, state, progression) {
+      ratio <- state$step / config$max_steps
+      size <- round(100 * ratio)
+      current_size <- file.size(file)
+      if (is.na(current_size)) file.create(file, showWarnings = FALSE)
+      if (size == 0L) return()
+
+      head <- sprintf("%g/%g: ", state$step, config$max_steps)
+      nhead <- nchar(head)
+      tail <- sprintf(" [%d%%]", round(100 * ratio))
+      ntail <- nchar(tail)
+      mid <- paste0(progression$message, "")
+      nmid <- nchar(mid)
+      padding <- size - (nhead + nmid + ntail)
+      if (padding <= 0) {
+        msg <- paste(head, mid, tail, sep = "")
+        if (padding < 0) msg <- substring(msg, first = 1L, last = size)
+      } else if (padding > 0) {
+        mid <- paste(c(mid, " ", rep(".", times = padding - 1L)), collapse = "")
+        msg <- paste(head, mid, tail, sep = "")
+      }
+      
+      cat(file = file, append = FALSE, msg)
+    }
+    
+    list(
+      initiate = function(config, state, progression, ...) {
+        set_file_size(config = config, state = state, progression = progression)
+      },
+      
+      update = function(config, state, progression, ...) {
+        set_file_size(config = config, state = state, progression = progression)
+      },
+      
+      finish = function(config, state, progression, ...) {
+        if (config$clear) {
+	  if (file_test("-f", file)) file.remove(file)
+	} else {
+          set_file_size(config = config, state = state, progression = progression)
+	}
+      }
+    )
+  })
+  
+  progression_handler("filesize", reporter, intrusiveness = intrusiveness, ...)
 }
