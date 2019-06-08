4c5a3bfdbf3dc7d858a682ae143ae67b2624967b
Henrik Bengtsson <hb@aroma-project.org>
2019-06-08 11:43:38 -0700

    with_progressr() gained argument 'delay_stdout' and 'delay_conditions' [#50]

--- R/with_progress.R
+++ R/with_progress.R
@@ -8,12 +8,19 @@
 #' @param cleanup If TRUE, all progression handlers will be shutdown
 #' at the end regardless of the progression is complete or not.
 #'
+#' @param delay_stdout If TRUE, standard output is captured and relayed
+#' at the end just before any captured conditions are relayed.
+#'
+#' @param delay_conditions A character vector specifying [base::condition]
+#' classes to be captured and relayed at the end after any captured
+#' standard output is relayed.
+#'
 #' @return Return nothing (reserved for future usage).
 #'
 #' @example incl/with_progress.R
 #'
 #' @export
-with_progress <- function(expr, handlers = getOption("progressr.handlers", txtprogressbar_handler()), cleanup = TRUE) {
+with_progress <- function(expr, handlers = getOption("progressr.handlers", txtprogressbar_handler()), cleanup = TRUE, delay_stdout = TRUE, delay_conditions = c("condition")) {
   stop_if_not(is.logical(cleanup), length(cleanup) == 1L, !is.na(cleanup))
   
   ## FIXME: With zero handlers, progression conditions will be
@@ -59,16 +66,78 @@ with_progress <- function(expr, handlers = getOption("progressr.handlers", txtpr
     })
   }
 
+  ## Captured stdout output and conditions
+  stdout_file <- NULL
+  conditions <- list()
+  if (delay_stdout || length(delay_conditions) > 0) {
+    ## Delay standard output?
+    if (delay_stdout) {
+      stdout_file <- rawConnection(raw(0L), open = "w")
+      sink(stdout_file, type = "output", split = FALSE)
+      on.exit({
+        sink(type = "output", split = FALSE)
+        stdout <- rawToChar(rawConnectionValue(stdout_file))
+        close(stdout_file)
+        if (length(stdout) > 0) cat(stdout, file = stdout())
+      })
+    }
+    
+    ## Delay conditions?
+    if (length(delay_conditions) > 0) {
+      on.exit({
+        if (length(conditions) > 0L) {
+          for (kk in seq_along(conditions)) {
+            c <- conditions[[kk]]
+            if (inherits(c, "message")) {
+              message(c)
+            } else if (inherits(c, "warning")) {
+              warning(c)
+            } else if (inherits(c, "condition")) {
+              signalCondition(c)
+            }
+          }
+        }
+      }, add = TRUE)
+    }
+  }
+
   ## Reset all handlers up start
   withCallingHandlers({
-    withRestarts({
-      signalCondition(control_progression("reset"))
-    }, muffleProgression = function(p) NULL)
+      withRestarts({
+        signalCondition(control_progression("reset"))
+      }, muffleProgression = function(p) NULL)
   }, progression = handler)
 
   ## Evaluate expression
-  withCallingHandlers(expr, progression = handler)
-
+  withCallingHandlers(
+    expr,
+    progression = handler,
+    condition = function(c) {
+      if (inherits(c, c("progression", "error"))) return()
+      if (inherits(c, delay_conditions)) {
+        ## Record
+        conditions[[length(conditions) + 1L]] <<- c
+        ## Muffle
+        if (inherits(c, "message")) {
+          invokeRestart("muffleMessage")
+        } else if (inherits(c, "warning")) {
+          invokeRestart("muffleWarning")
+        } else if (inherits(c, "condition")) {
+          ## If there is a "muffle" restart for this condition,
+          ## then invoke that restart, i.e. "muffle" the condition
+          restarts <- computeRestarts(c)
+          for (restart in restarts) {
+            name <- restart$name
+            if (is.null(name)) next
+            if (!grepl("^muffle", name)) next
+            invokeRestart(restart)
+            break
+          }
+        }
+      }
+    }
+  )
+  
   ## Success
   status <- "ok"
   
