d3c33597a0b3be6a1419cab9e7cfcce317a7bf05
Henrik Bengtsson <hb@aroma-project.org>
2020-11-10 18:47:30 -0800

    CLEANUP: Drop unneeded code

--- tests/zzz,purrr.R
+++ tests/zzz,purrr.R
@@ -1,7 +1,6 @@
 source("incl/start.R")
 
 if (requireNamespace("purrr", quietly = TRUE)) {
-  future::plan("multiprocess")
   with_progress({
     p <- progressor(4)
     y <- purrr::map(3:6, function(n) {
