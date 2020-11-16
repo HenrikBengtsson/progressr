83edb3ca502724006ef0cc45e636a7ee315f1b84
Henrik Bengtsson <hb@aroma-project.org>
2020-11-16 14:04:58 -0800

    Test buffering and flusing when there are (i) the exact number expected of updated, too few and too many

--- tests/globals,relay.R
+++ tests/globals,relay.R
@@ -24,28 +24,31 @@ for (kk in seq_along(handlers)) {
 
   for (type in c("message", "warning")) {
     message(sprintf("  - stdout + %ss", type))
-    truth <- c()
-    relay <- record_relay({
-      p <- progressor(n)
-      for (ii in seq_len(n)) {
-        ## Zero-amount progress with empty message
-        p(amount = 0)
-        msg <- sprintf("ii = %d", ii)
-        ## Zero-amount progress with non-empty message
-        p(message = msg, amount = 0)
-        truth <<- c(truth, msg)
-        cat(msg, "\n", sep = "")
-        ## Signal condition
-        do.call(type, args = list(msg))
-        Sys.sleep(delay)
-        ## One-step progress with non-empty message
-        p(message = sprintf("(%s)", paste(letters[1:ii], collapse=",")))
-      }
-    }, classes = type)
-    stopifnot(
-      identical(relay$stdout, truth),
-      identical(gsub("\n$", "", relay$msgs), truth)
-    )
+    for (delta in c(0L, +1L, -1L)) {
+      message(sprintf("    - delta = %+d", delta))
+      truth <- c()
+      relay <- record_relay({
+        p <- progressor(n)
+        for (ii in seq_len(n + delta)) {
+          ## Zero-amount progress with empty message
+          p(amount = 0)
+          msg <- sprintf("ii = %d", ii)
+          ## Zero-amount progress with non-empty message
+          p(message = msg, amount = 0)
+          truth <<- c(truth, msg)
+          cat(msg, "\n", sep = "")
+          ## Signal condition
+          do.call(type, args = list(msg))
+          Sys.sleep(delay)
+          ## One-step progress with non-empty message
+          p(message = sprintf("(%s)", paste(letters[1:ii], collapse=",")))
+        }
+      }, classes = type)
+      stopifnot(
+        identical(relay$stdout, truth),
+        identical(gsub("\n$", "", relay$msgs), truth)
+      )
+    } ## for (delta ...)
   } ## for (signal ...)
 
   message(sprintf("* Handler %d ('%s') of %d ... done", kk, name, length(handlers)))
