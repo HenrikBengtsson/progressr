9662d3636b637827ad3774117b98bb28d02a5ca4
Henrik Bengtsson <hb@aroma-project.org>
2022-04-19 20:16:27 -0700

    Only baby steps to add a 'cli' handler [ci skip]

---
+++ R/handler_cli.R
@@ -0,0 +1,157 @@
+#' Progression Handler: Progress Reported via 'cli' Progress Bars (Text) in the Terminal
+#'
+#' A progression handler for [cli::cli_progress_bar()].
+#'
+#' @inheritParams make_progression_handler
+#'
+#' @param format (character string) The format of the progress bar.
+#'
+#' @param show_after (numeric) Number of seconds to wait before displaying
+#' the progress bar.
+#'
+#' @param \ldots Additional arguments passed to [make_progression_handler()].
+#'
+#' @section Requirements:
+#' This progression handler requires the \pkg{cli} package.
+#'
+#' @section Appearance:
+#' Below is how this progress handler renders by default at 0%, 30% and 99%
+#' progress:
+#' 
+#' With `handlers(handler_cli())`:
+#' ```r
+#' - [-------------------------------------------------]   0% 
+#' \ [====>--------------------------------------------]  10% 
+#' | [================================================>]  99% 
+#' ```
+#'
+#' If the progression updates have messages, they will appear like:
+#' ```r
+#' - [-----------------------------------------]   0% Starting
+#' \ [===========>----------------------------]  30% Importing
+#' | [=====================================>]  99% Summarizing
+#' ```
+#'
+#' @example incl/handler_cli.R
+#'
+#' @export
+cli_progress <- function(format = "{cli::pb_spin} {cli::pb_bar} {cli::pb_percent} {cli::pb_status}", show_after = 0.0, intrusiveness = getOption("progressr.intrusiveness.terminal", 1), target = "terminal", ...) {
+  ## Additional arguments passed to the progress-handler backend
+  backend_args <- handler_backend_args(...)
+
+  if (!is_fake("handler_cli")) {
+    progress_bar <- cli::cli_progress_bar
+    
+    erase_progress_bar <- function(pb) {
+      if (pb$finished) return()
+      private <- get_private(pb)
+      private$clear_line(private$width)
+      private$cursor_to_start()
+    }
+    redraw_progress_bar <- function(pb, tokens = list()) {
+      if (pb$finished) return()
+      private <- get_private(pb)
+      private$last_draw <- ""
+      private$render(tokens)
+    }
+  } else {
+    progress_bar <- list(
+      new = function(...) list(
+        finished = FALSE,
+        tick = function(...) NULL,
+        update = function(...) NULL
+      )
+    )
+    get_private <- function(pb) NULL
+    erase_progress_bar <- function(pb) NULL
+    redraw_progress_bar <- function(pb, tokens = list()) NULL
+  }
+
+  reporter <- local({
+    pb <- NULL
+    
+    make_pb <- function(format, total, clear, show_after, ...) {
+      if (!is.null(pb)) return(pb)
+      stop_if_not(
+        is.character(format), length(format) == 1L,
+        is.numeric(total), length(total) == 1L,
+        is.logical(clear), length(clear) == 1L,
+        is.numeric(show_after), length(show_after) == 1L
+      )
+      args <- c(list(total = total, format = format, clear = clear,
+                     show_after = show_after, ...), backend_args)
+      pb <<- do.call(progress_bar, args = args)
+      pb
+    }
+
+    last_tokens <- list()
+    pb_tick <- function(pb, delta = 0, message = NULL, ...) {
+      if (isTRUE(pb$finished)) return()
+      
+      tokens <- list(message = paste0(message, ""))
+      last_tokens <<- tokens
+      if (delta < 0) return()
+      pb$tick(delta, tokens = tokens)
+    }
+
+    pb_update <- function(pb, ratio, ...) {
+      if (isTRUE(pb$finished)) return()
+      
+      pb$update(ratio = ratio, ...)
+    }
+
+    list(
+      reset = function(...) {
+        pb <<- NULL
+      },
+
+      hide = function(...) {
+        if (is.null(pb)) return()
+        erase_progress_bar(pb)
+      },
+
+      unhide = function(...) {
+        if (is.null(pb)) return()
+        redraw_progress_bar(pb, tokens = last_tokens)
+      },
+
+      interrupt = function(config, state, progression, ...) {
+        if (is.null(pb)) return()
+        msg <- getOption("progressr.interrupt.message", "interrupt detected")
+        msg <- paste(c("", msg, ""), collapse = "\n")
+        cat(msg, file = stderr())
+      },
+
+      initiate = function(config, state, progression, ...) {
+        if (!state$enabled || config$times == 1L) return()
+        stop_if_not(is.null(pb))
+        make_pb(format = format, total = config$max_steps,
+                clear = config$clear, show_after = config$enable_after)
+        pb_tick(pb, 0, message = state$message)
+      },
+        
+      update = function(config, state, progression, ...) {
+        if (!state$enabled || config$times <= 2L) return()
+        make_pb(format = format, total = config$max_steps,
+                clear = config$clear, show_after = config$enable_after)
+        if (inherits(progression, "sticky") && length(state$message) != 0)
+          pb$message(state$message)
+        pb_tick(pb, state$delta, message = state$message)
+      },
+        
+      finish = function(config, state, progression, ...) {
+        ## Already finished?
+        if (is.null(pb)) return()
+        if (!pb$finished) {
+          make_pb(format = format, total = config$max_steps,
+                  clear = config$clear, show_after = config$enable_after)
+          reporter$update(config = config, state = state, progression = progression, ...)
+          if (config$clear) pb_update(pb, ratio = 1.0)
+        }
+        pb <<- NULL
+      }
+    )
+  })
+
+  make_progression_handler("cli", reporter, intrusiveness = intrusiveness, target = target, ...)
+}
